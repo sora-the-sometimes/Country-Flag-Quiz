1dcdeeacf8720f76d7d0c2fef3fcc97c89054c75
Sora <kaithemaido@gmail.com>
2023-01-09 10:08:06 -0600

    Added Country Flag Images, plus Question and Answer structs

---
+++ Country Flag Quiz/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Country Flag Quiz
+//
+//  Created by Sora on 1/9/23.
+//
+
+import Foundation
+struct Answer: Identifiable {
+    var id = UUID()
+    var text: String
+    var isCorrect: Bool
+}
+
+struct Question: Identifiable {
+    var id = UUID()
+    var correctAnswer: Answer
+    var incorrectAnswer: [Answer]
+}
+
