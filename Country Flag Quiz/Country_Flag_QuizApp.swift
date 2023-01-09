18e7678abeef9863cb3e21619ef4ec6c3037a2b4
Sora <kaithemaido@gmail.com>
2023-01-09 09:50:37 -0600

    Initial Commit

---
+++ Country Flag Quiz/Country_Flag_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_QuizApp.swift
+//  Country Flag Quiz
+//
+//  Created by Sora on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
